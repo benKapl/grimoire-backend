38fc79049475550424647e0b5322f5e65485c681
benKapl <benji1.kaplan@gmail.com>
2024-11-10 20:13:11 +0100

    made POC code execution on back

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ var router = express.Router();
 const JDOODLE_URL = "https://api.jdoodle.com/v1/execute"
 
 /* GET home page. */
-router.get('/test', async (req, res) => {
+router.post('/test', async (req, res) => {
+  const { script } = req.body
+
   const request = {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    script: "console.log(10 + 13)",
-    language: "nodejs",
+    script: script,
+    language: "python3",
     versionIndex: "0"
   }
-
 
   const response = await fetch(JDOODLE_URL, {
     method: "POST",

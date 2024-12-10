fa20d5b729c9c7124c0803275564ad2f7043e3ed
NicoJoub <105387349+NicoJoub@users.noreply.github.com>
2024-12-10 09:36:55 +0100

    add token for Notelinks

--- routes/notes.js
+++ routes/notes.js
@@ -6,6 +6,31 @@ const { checkBody } = require('../modules/checkBody');
 const Note = require('../models/notes');
 const User = require('../models/users');
 
+/** Get all note with title and ids*/
+router.get('/user/:token', async (req, res) => {
+  try {
+    const { token } = req.params;
+
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return res.json({ result: false, error: 'User not found' });
+    }
+
+    const notes = await Note.find({ user: user._id });
+
+    res.json({
+      result: true,
+      notes: notes.map((note) => ({
+        id: note._id,
+        title: note.title,
+      })),
+    });
+  } catch (err) {
+    res.json({ result: false, error: err.message });
+  }
+});
+
 /** Create note from its ID in database */
 router.get('/:noteId', async (req, res) => {
   try {
@@ -23,15 +48,15 @@ router.get('/:noteId', async (req, res) => {
 });
 
 /** Create a new note in database */
-router.post("/", async (req, res) => {
-  const isBodyValid = checkBody(req.body, ["token"]);
-  if (!isBodyValid) throw new Error("Missing or empty body parameter")
-  
-  try  {
-    const { token } = req.body
-    const user = await User.findOne({ token })
-    console.log(user)
-    if (!user) throw new Error("User not found")
+router.post('/', async (req, res) => {
+  const isBodyValid = checkBody(req.body, ['token']);
+  if (!isBodyValid) throw new Error('Missing or empty body parameter');
+
+  try {
+    const { token } = req.body;
+    const user = await User.findOne({ token });
+    console.log(user);
+    if (!user) throw new Error('User not found');
 
     const newNote = await Note.create({
       title: 'Nouvelle note',
@@ -47,23 +72,6 @@ router.post("/", async (req, res) => {
 
     if (!newNote) throw new Error('Could not create stack');
     res.json({ result: true, note: newNote });
-  } catch (err) {
-    res.json({ result: false, error: err.message });
-  }
-});
-
-/** Get all note with title and ids*/
-router.get('/', async (req, res) => {
-  try {
-    const notes = await Note.find();
-
-    const notesList = notes.map((note) => {
-      return {
-        id: note._id,
-        title: note.title,
-      };
-    });
-    res.json({ result: true, notes: notesList });
   } catch (err) {
     res.json({ result: false, error: err.message });
   }

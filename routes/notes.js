fc8ae885a76f34276107d9f1da60a5ce8c10d9ab
Marvin <timodentmarvin@live.fr>
2024-12-10 10:51:05 +0100

    new route query search

--- routes/notes.js
+++ routes/notes.js
@@ -8,4 +8,27 @@ router.get('/', function(req, res, next) {
   res.send('notes');
 });
 
+
+router.get('/search/:query', async (req, res, next)=> {
+  try {
+      const query = req.params.query;
+      
+      // Vérifier si query est vide
+      if (!query) {
+          return res.status(400).json({ message: 'Query is required' });
+      }
+
+      // Utiliser une expression régulière pour une recherche partielle (insensible à la casse)
+      const notes = await Note.find({
+          title: { $regex: `^${query}`, $options: 'i' } // 'i' rend la recherche insensible à la casse
+      });
+
+      res.status(200).json(notes);
+  } catch (error) {
+      console.error('Error fetching notes:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;

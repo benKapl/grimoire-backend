5bcbad72ebae779729ac87da12ae087e96b5dbd9
NicoJoub <105387349+NicoJoub@users.noreply.github.com>
2024-12-11 14:40:43 +0100

    router delete check

--- routes/notes.js
+++ routes/notes.js
@@ -42,32 +42,34 @@ router.get('/:noteId', async (req, res) => {
 
     if (!note) throw new Error('Could not get note');
     res.json({ result: true, note: note });
-  } catch (err) { 
+  } catch (err) {
     res.json({ result: false, error: err.message });
   }
 });
 
 /** Create a new note in database */
-router.post("/", async (req, res) => {
-  const isBodyValid = checkBody(req.body, ["token"]);
-  if (!isBodyValid) throw new Error("Missing or empty body parameter")
-  
-  try  {
-    const { token } = req.body
+router.post('/', async (req, res) => {
+  const isBodyValid = checkBody(req.body, ['token']);
+  if (!isBodyValid) throw new Error('Missing or empty body parameter');
 
-    const user = await User.findOne({ token })
-    if (!user) throw new Error("User not found")
+  try {
+    const { token } = req.body;
+
+    const user = await User.findOne({ token });
+    if (!user) throw new Error('User not found');
 
     const newNote = await Note.create({
       title: 'Nouvelle note',
       createdAt: Date.now(),
       updatedAt: Date.now(),
-      blocs: [{
-        position: 0,
-        type: "text",
-        value: "",
-        language: null,
-      }],
+      blocs: [
+        {
+          position: 0,
+          type: 'text',
+          value: '',
+          language: null,
+        },
+      ],
       forwardNotes: [],
       backwardNotes: [],
       isBookmarked: false,
@@ -83,25 +85,26 @@ router.post("/", async (req, res) => {
 });
 
 /** Save note when modified*/
-router.put("/", async (req, res) => {
-  const isBodyValid = checkBody(req.body, ["noteId"]);
-  if (!isBodyValid) throw new Error("Missing or empty body parameter")
+router.put('/', async (req, res) => {
+  const isBodyValid = checkBody(req.body, ['noteId']);
+  if (!isBodyValid) throw new Error('Missing or empty body parameter');
   try {
-    const { noteId, noteData } = req.body
+    const { noteId, noteData } = req.body;
 
     // const user = await User.findOne({ token })
     // if (!user) throw new Error("User not found")
-    const note = await Note.updateOne({ _id: noteId }, {
-      title: noteData.title,
-      updatedAt: Date.now(),
-      blocs: noteData.blocs,
-    });
-    res.json({ result: true })
-
-  } catch(err) {
-    res.json({ result: false, error: err.message })
+    const note = await Note.updateOne(
+      { _id: noteId },
+      {
+        title: noteData.title,
+        updatedAt: Date.now(),
+      }
+    );
+    res.json({ result: true });
+  } catch (err) {
+    res.json({ result: false, error: err.message });
   }
-})
+});
 
 /** Get all note with title and ids*/
 router.get('/', async (req, res) => {
@@ -120,27 +123,48 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-router.get('/search/:query', async (req, res, next)=> {
+router.get('/search/:query', async (req, res, next) => {
   try {
-      const query = req.params.query;
-      
-      // Vérifier si query est vide
-      if (!query) {
-          return res.status(400).json({ message: 'Query is required' });
-      }
+    const query = req.params.query;
 
-      // Utiliser une expression régulière pour une recherche partielle (insensible à la casse)
-      const notes = await Note.find({
-          title: { $regex: `^${query}`, $options: 'i' } // 'i' rend la recherche insensible à la casse
-      });
+    // Vérifier si query est vide
+    if (!query) {
+      return res.status(400).json({ message: 'Query is required' });
+    }
 
-      res.status(200).json(notes);
+    // Utiliser une expression régulière pour une recherche partielle (insensible à la casse)
+    const notes = await Note.find({
+      title: { $regex: `^${query}`, $options: 'i' }, // 'i' rend la recherche insensible à la casse
+    });
+
+    res.status(200).json(notes);
   } catch (error) {
-      console.error('Error fetching notes:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
+    console.error('Error fetching notes:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
+router.delete('/delete/:noteId', async (req, res) => {
+  try {
+    const { noteId } = req.params;
+    if (!noteId) {
+      return { result: false, error: 'Invalid ID' };
+    }
+    // Trouve la note avant de la supprimer
+    const noteToDelete = await Note.findById(noteId);
+    if (!noteToDelete) {
+      return res.status(404).json({ result: false, error: 'Note not found' });
+    }
+    // Supprime la note
+    await Note.deleteOne({ _id: noteId });
+    res.json({
+      result: true,
+      message: 'Note deleted successfully',
+    });
+  } catch (error) {
+    console.error('Error deleting note:', error);
+    res.status(500).json({ result: false, error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;

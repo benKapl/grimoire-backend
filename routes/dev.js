817dbae9996e8c28282f49e003ba57e6a8d016ca
Your name <Your email>
2024-12-13 17:09:28 +0100

    routes tag

--- routes/dev.js
+++ routes/dev.js
@@ -28,4 +28,20 @@ router.post('/dev', async (req, res) => {
    res.json({result: true, data: dataApi});
 })
 
+router.get('/',async ( req, res) => {
+  try {
+    
+   const lang = await DevLang.find()
+   console.log("lang", lang)
+   if (lang && lang.length > 0) {
+    return res.json({ result: true, message: "Langages récupérés avec succès", lang });
+  } else {
+    return res.json({ result: false, message: "Aucun langage trouvé" });
+  }
+
+   } catch(error) {
+    return res.json({ result: false, error: error.message });
+  }
+
+})
 module.exports = router;
